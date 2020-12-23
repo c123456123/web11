121b0dc158a7de28380cd510a2b49555bfae12f0
c123456 <843726309@qq.com>
2020-12-23 11:34:46 +0800

    练习

---
+++ 第十三周课程/vue/vue01/src/components/My.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="wrapper">
+  <div id="name">江西软件大学</div>
+  <div id="content">江西软件大学简介</div>
+  <div v-for="index in lists" :key="index">{{index.name}}</div>
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      lists:[{name:"张三"},{name:"李四"}],
+      information:null
+    }
+  }
+
+}
+
+</script>
+
+<style  scoped lang="less" >
+#wrapper{
+  background-color: red;
+}
+#name{
+  background-color: orange;
+  font: 5em sans-serif;
+}
+</style>
+
